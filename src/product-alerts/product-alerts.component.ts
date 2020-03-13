47b12ad1db9868e6adbc25c50f3cefd73d797976
rahuofu <rich.hansen@utah.edu>
2020-03-13 10:49:23 -0600

    1 file modified, 3 added

---
+++ src/product-alerts/product-alerts.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
+
+import { product } from '../product';
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+export class ProductAlertsComponent implements OnInit {
+
+  @Input() product;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
