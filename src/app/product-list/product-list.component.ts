9368359bf3eda44c0df58008424221b6a84d4f95
rahuofu <rich.hansen@utah.edu>
2020-03-13 12:07:05 -0600

    1 file modified, 3 added, 3 deleted

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 import { products } from '../products';
-import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductAlertsComponent } from '../product-alerts/product-alerts.component';
 
 @Component({
   selector: 'app-product-list',
@@ -13,6 +13,10 @@ export class ProductListComponent {
 
   share() {
     window.alert('The product has been shared!');
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
   }
 }
 
